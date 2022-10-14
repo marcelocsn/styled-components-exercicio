742e7113b048f8dc928452a77d4857fad8e9b3ce
csnicolela.marcelo@gmail.com <csnicolela.marcelo@gmail.com>
2022-10-14 15:38:42 -0300

    Exercicio de styled componentes - React - realizado dia 14-10-2022

---
+++ src/Componentes/Nav.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const EstiloVideo=styled.div`
+    display:grid;
+    grid-template-rows: 3fr 1fr;
+    grid-template-columns: 1fr;
+    border-style: solid;
+    border-width: thin;
+    align-itens: start;
+    justify-itens: stretch;
+
+`;
